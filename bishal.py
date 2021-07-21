d71564ce1de7a6dca679b3d54b125f76821bc2de
unknown <studentmadan@gmail.com>
2021-07-21 06:30:06 +0545

    changed codes

--- bishal.py
+++ bishal.py
@@ -1,11 +1,6 @@
-print ("hi")
-
-
-print("added by Anup Babu Shrestha")
-#This statement print hi.
-print ("hi")
-print ("Manish")
-
 #this statement print hi
 print ("hi")
 print ("hello")
+
+print("Welcome to Python Class")
+
